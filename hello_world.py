b22c78d4c51f8640e8a2c40d7513268dbb18ba0a
gnavneetg <47924966+gnavneetg@users.noreply.github.com>
2019-04-02 20:20:07 -0400

    added Patricia last name

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,9 @@
-print("Hello Team # 5 🕺🏽💃🕺🏽🕺🏽")
+﻿print("Hello Team # 5 🕺🏽💃🕺🏽🕺🏽")
 print("Alex")
 for x in range(5):
     print("way to go Group5)
 
-print("Hi, this is Patricia 😎")
+print("Hi, this is Patricia Castro😎")
 
 
 
