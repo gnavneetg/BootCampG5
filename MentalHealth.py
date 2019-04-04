4e3f13be1ff5a12e2d920c02860db3af93d18078
Patricia Castro <patricia.castro.upr@gmail.com>
2019-04-04 19:36:06 -0400

    Patcasrom changes

--- MentalHealth.py
+++ MentalHealth.py
@@ -3,6 +3,8 @@ print("Group 5 project name is Mental health data analyis in TECH")
 
 
 
- 1. Do people that are self employed have a higher risk for mental illness?
+print( "Do people that are self employed have a higher risk for mental illness?")
+
+
 
 
