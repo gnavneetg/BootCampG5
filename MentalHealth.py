a152079b7aaab479eeca8f9480322ff6eee5926b
marez111 <myrealgeorgia@gmail.com>
2019-04-04 19:26:12 -0400

    added last question by mauricio

--- MentalHealth.py
+++ MentalHealth.py
@@ -1,1 +1,7 @@
 print("Group 5 project name is Mental health data analyis in TECH")
+
+
+
+# This is Mauricio: I'll do the last question from the slack file
+
+last_question = "'If you have revealed a mental health issue to a client or business contact, do you believe this has impacted you negatively?'"
